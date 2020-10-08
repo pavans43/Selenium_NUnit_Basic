11eee1fb44af8725baab2a6dbbefeaec0b60bf41
Pavan Padiyala <PadiyalP@healthshareltd.net>
2020-10-08 13:48:56 +1300

    NUnit Selenium Project Startup

---
+++ SeleniumNunitFramework/ExtentReportsHelper.cs
@@ -0,0 +1,87 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace SeleniumNunitFramework
+{
+    public class ExtentReportsHelper
+    {
+        public IWebDriver driver;
+        public ExtentReports extent { get; set; }
+        public ExtentV3HtmlReporter reporter { get; set; }
+        public ExtentTest test { get; set; }
+
+  public ExtentReportsHelper()
+    {
+
+            extent = new ExtentReports();
+            reporter = new ExtentV3HtmlReporter(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),"ExtentReports.html"));
+            reporter.Config.DocumentTitle = "Test Autoamtion Execution Report";
+            reporter.Config.ReportName = "Pavan Padiyala";
+            reporter.Config.Theme = AventStack.ExtentReports.Reporter.Configuration.Theme.Dark;
+            extent.AttachReporter(reporter);
+            extent.AddSystemInfo("Sys Test", "demo");
+            extent.AddSystemInfo("Envi", "DEVSIT");
+            extent.AddSystemInfo("MachineName", Environment.MachineName);
+            extent.AddSystemInfo("OS", Environment.OSVersion.VersionString);
+
+
+        }
+        //createa a test 		
+        public void CreateTest(string testName)
+        {
+            test = extent.CreateTest(testName);
+        }
+        //sets test steps status to Pass 
+        public void SetStepStatusPass(string stepDescription)
+        {
+
+            test.Log(Status.Pass, stepDescription);
+        }
+        //set test step status to warning 
+        public void SetStepStatusWarning(string stepDescription)
+        {
+
+            test.Log(Status.Warning, stepDescription);
+        }
+        //set overall test status to Pass 
+        public void SetTestStatusPass()
+        {
+
+            test.Pass("Test Executed Sucessfully!");
+        }
+        //set overall tests status to Fail with an error 
+        public void SetTestStatusFail(string message = null)
+        {
+
+            var printMessage = "<p><b>Test FAILED!</b></p>";
+            if (!string.IsNullOrEmpty(message))
+            {
+                printMessage += $"Message: <br>{message}<br>";
+            }
+            test.Fail(printMessage);
+        }
+        //adds exception screenshot to Extent Report 
+        public void AddTestFailureScreenshot(string base64ScreenCapture)
+        {
+
+            test.AddScreenCaptureFromBase64String(base64ScreenCapture, "Screenshot on Error:");
+        }
+        //set steps to skip 
+        public void SetTestStatusSkipped()
+        {
+            test.Skip("Test skipped!");
+        }
+        public void Close()
+        {
+            extent.Flush();
+        }
+    }
+
+}
+
---
+++ SeleniumNunitFramework/HomePage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+using System.IO;
+
+
+
+namespace SeleniumNunitFramework
+{
+    [TestFixture]
+    class Home
+    {
+       public  IWebDriver driver = null;
+        public IConfiguration config;
+        public ExtentReportsHelper extent;
+        
+        [OneTimeSetUp]
+        public void Intialize()
+        {
+            driver = new ChromeDriver(AppDomain.CurrentDomain.BaseDirectory);
+            driver.Manage().Window.Maximize();
+            config = new ConfigurationBuilder().AddJsonFile("config.json").Build();
+            extent = new ExtentReportsHelper();
+        }
+
+        [Test]
+        public void HomePage()
+        {
+            driver.Navigate().GoToUrl(config["ShopUrl"]);
+            Console.WriteLine("APplication Launched successfully");
+
+        }
+        [Test]
+        public void CreateStore()
+        {
+            driver.Navigate().GoToUrl(config["ShopUrl"]);
+            driver.FindElement(By.XPath("//a[contains(text(),'Create my store')]")).Click();
+            System.Threading.Thread.Sleep(3000);
+
+
+        }
+
+
+        [TearDown]
+        public void CloseDriver()
+        {
+            driver.Quit();
+
+        }
+
+    }
+}
